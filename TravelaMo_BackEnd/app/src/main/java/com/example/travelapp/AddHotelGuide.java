8d5caf7901d1df4280ee7af8704347d174e9f5ba
expertfox <rajithpriv@gmail.com>
2019-09-21 01:23:49 +0530

    Hotel Add

--- TravelaMo_BackEnd/app/src/main/java/com/example/travelapp/AddHotelGuide.java
+++ TravelaMo_BackEnd/app/src/main/java/com/example/travelapp/AddHotelGuide.java
@@ -8,19 +8,24 @@ import android.view.View;
 
 public class AddHotelGuide extends AppCompatActivity {
 
+    Intent intentget;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_hotel_guide);
+        this.intentget = getIntent();
     }
 
     public void guide(View v) {
         Intent intent = new Intent(this, AddActivity.class);
+        intent.putExtra("district",this.intentget.getStringExtra("district") );
         startActivity(intent);
     }
 
     public void hotel(View v) {
         Intent intent = new Intent(this, add_hotel.class);
+        intent.putExtra("district",this.intentget.getStringExtra("district") );
         startActivity(intent);
     }
 

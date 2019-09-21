27d2c9026f165efed5781ad1e8ecca84b4a8a18d
Hansika_Samaraweera <hansika.m99@gmail.com>
2019-09-21 18:50:27 -0230

    #

--- TravelMo/app/src/main/java/com/example/travelmo/HotelBookingDetailEdit.java
+++ TravelMo/app/src/main/java/com/example/travelmo/HotelBookingDetailEdit.java
@@ -24,6 +24,7 @@ public class HotelBookingDetailEdit extends AppCompatActivity {
     DatabaseReference dref;
     UserDetailForHotelReserv hotel;
     String chiId;
+    String pasindu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
